23ab1f59d969c90d0167eaab52b225de1b0fedc3
akki444 <igormerkulov1@ukr.net>
2018-12-10 19:07:05 +0200

    add new func

--- km-83/Merkulov_Ihor/workshop2/homework/task1.py
+++ km-83/Merkulov_Ihor/workshop2/homework/task1.py
@@ -1,7 +1,23 @@
-"""
-Тут написати умову до завдання
-"""
+file = open("data/billboard_lyrics_1964-2015.csv")
+info = file.read()
+list1 = []
+i= 0
+while i != 3000:
+    list1.append(info.splitlines()[i].split(","))
+    print(i)
+    i = i + 1
+dataset = {
+
+}
+del list1[0]
+for i1 in list1:
+    dataset[i1[2]] = {i1[1]:{
+        "Year": i1[3],
+        "Rank": i1[0],
+        "Lyrics": i1[4],
+        "Source": i1[5]
+    }}
 
 
 
-#TODO
+print(dataset['"bon jovi"'])
